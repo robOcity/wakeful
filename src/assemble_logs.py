1e0e1e54833184492b06fa8f2af8ffcb7a6231f2
rob <robert.osterburg@gmail.com>
2017-12-31 12:47:13 -0700

    adding bro eda logs, eda notebook and improved assemble_logs.py

--- src/assemble_logs.py
+++ src/assemble_logs.py
@@ -7,9 +7,9 @@ import click
 
 @click.command()
 @click.option('--src_dir', help='Directory to recursively search for log files')
-@click.option('--dest_file', help='Path to output file with filename may be relative or absolute')
+@click.option('--dest_dir', help='Path to output directory that may be relative or absolute')
 @click.option('--log_type', default='dns', help='Type of Bro log file to process')
-def assemble_logs(src_dir, dest_file, log_type):
+def assemble_logs(src_dir, dest_dir, log_type):
     """
     Ingests Bro Network Security Monitor logs and concatentates them into a
     a single output file suitible.  Optionally decompresses the files before
@@ -19,18 +19,26 @@ def assemble_logs(src_dir, dest_file, log_type):
     # recursively get all the file in directoreis under src_dir
     path = os.path.join(src_dir, '*', log_type.lower() + '**.gz')
     num_files, num_lines = 0, 0
-    with open(dest_file, 'wb') as outfile:
+    dest_file = os.path.join(dest_dir, log_type.lower() + '_all.log')
+    remove_existing_file(dest_file)
+    with open(dest_file, 'ab') as outfile:
         for src_name in glob.iglob(path, recursive=True):
             with gzip.open(src_name, 'rb') as infile:
                 for line in infile:
                     outfile.write(line)
                     num_lines += 1
+            f_state = f'outfile: {str({outfile})}'
             num_files += 1
     # click side-effect prevents this running in main, or printing the returned values too. WEIRD!!
     click.echo(f'{num_files} files with {num_lines} lines processed')
     return num_files, num_lines
 
 
+def remove_existing_file(filepath):
+    """Removes the file if it exists"""
+    if os.path.exists(filepath):
+        os.remove(filepath)
+
+
 if __name__ == '__main__':
     num_files, num_lines = assemble_logs()
-    click.echo(f'{num_files} files with {num_lines} lines processed')
