ec8c9067adb452c503d6dc4f065fdad02da0ae4f
rob <robert.osterburg@gmail.com>
2018-01-07 21:30:08 -0700

    adding docstrings and moving eda notebook up a directory

--- wakeful/log_munger.py
+++ wakeful/log_munger.py
@@ -4,11 +4,23 @@ from bat.log_to_dataframe import LogToDataFrame
 
 
 def bro_log_to_df(file_path):
+    """
+    Load a Bro log in to a pandas DataFrame object.
+    :param file_path: Log file
+    :return: pandas DataFrame object
+    """
     return LogToDataFrame(file_path)
 
 
-def bro_logs_to_df(top_level_dir, log_kind):
-    logs = make_log_list(top_level_dir, log_kind)
+def bro_logs_to_df(top_level_dir, log_type):
+    """
+    Recursively find log files and load them into a DataFrame object.  Only log files
+    that contain the given log name will be loaded.
+    :param top_level_dir: Root directory for the recursive search
+    :param log_type: Name of the Bro log file to load (e.g., dns)
+    :return:
+    """
+    logs = make_log_list(top_level_dir, log_type)
     df = pd.DataFrame()
     for log in logs:
         log_df = bro_log_to_df(log)
@@ -19,16 +31,34 @@ def bro_logs_to_df(top_level_dir, log_kind):
     return df
 
 
-def make_log_list(top_level_dir, log_kind):
+def make_log_list(root_dir, log_type):
+    """
+    Builds a list of Bro log files by recursively searching all subdirectories under
+    the root directory.
+    :param top_level_dir: Root directory for the recursive search
+    :param log_type: Name of the Bro log file to load (e.g., dns)
+    :return: List of paths to log file that were found
+    """
     paths = []                                                           
-    for rs, ds, fs in os.walk(top_level_dir):
-        paths.extend([os.path.join(top_level_dir, f) for f in fs if log_kind in f])
+    for rs, ds, fs in os.walk(root_dir):
+        paths.extend([os.path.join(root_dir, f) for f in fs if log_type in f])
     return paths
 
 
 def df_to_hdf5(df, path, append=False):
+    """
+    Save the DataFrame object as an HDF5 file.
+    :param df: DataFrame to save as a file
+    :param path: Path to directory of where to save the file
+    :param append: 'True' appends to the file, 'False' overwrites it
+    """
     df.to_hdf(path, 'table', append=append)
 
 
 def hdf5_to_df(path):
+    """
+    Retrieve the persisted DataFrame object from the HDF5 file.
+    :param path: Path to directory of where to save the file
+    :return: Retrieved DataFrame object
+    """
     return pd.read_hdf(path, 'table')
