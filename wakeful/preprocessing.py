264cc00d57aaeaed074b1df29816d751e5abf561
rob <robert.osterburg@gmail.com>
2018-01-19 06:36:52 -0700

    resample only if samples > min#

--- wakeful/preprocessing.py
+++ wakeful/preprocessing.py
@@ -8,7 +8,7 @@ from . import log_munger, metrics
 
 
 def pipeline(dir_pairs, log_types):
-
+    MIN_SAMPLES = 100
     # TODO update functions that mutate dataframe to not have return statements
     kv_pairs = []
     for mal_dir, norm_dir in dir_pairs:
@@ -28,7 +28,7 @@ def pipeline(dir_pairs, log_types):
             mal_df = log_munger.bro_logs_to_df(mal_dir, log_type, logs=logs)
 
             # add class labels to the data
-            label_data(norm_df, mal_df)
+            norm_df, mal_df = label_data(norm_df, mal_df)
             df = combine_dfs(norm_df, mal_df)
 
             # metrics depend on specific data fields
@@ -38,15 +38,24 @@ def pipeline(dir_pairs, log_types):
                 df['query_entropy'] = df['query'].apply(metrics.calc_entropy)
 
             # rebalance the training data
+            if min(get_class_counts(df)) < MIN_SAMPLES:
+                continue
             train, test = train_test_rebalance_split(df)
-            kv_pairs.append((norm_base + '-' + mal_base + '-' + log_type, (train, test)))
+            kv_pairs.append((mal_base + '-' + norm_base + '-' + log_type, (train, test)))
 
     return dict(kv_pairs)
 
 
-def label_data(norm_df, mal_df):
-    norm_df['label'] = 0
-    mal_df['label'] = 1
+def label_data(norm_df, mal_df, label='label'):
+    norm_df[label] = 0
+    mal_df[label] = 1
+    return norm_df, mal_df
+
+
+def get_class_counts(df, label='label', pos_class=1, neg_class=0):
+    pos_count = len(df[label][df[label] == pos_class])
+    neg_count = len(df[label][df[label] == neg_class])
+    return pos_count, neg_count
 
 
 def combine_dfs(norm_df, mal_df):
