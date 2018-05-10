b11a155065df24233be07e06e41a33298b57bc4e
rob <robert.osterburg@gmail.com>
2018-05-10 15:18:22 -0600

    adding todo

--- wakeful/scoring.py
+++ wakeful/scoring.py
@@ -14,14 +14,13 @@ def print_scores(estimator_name=None, data_name=None, estimator=None, X_test=Non
                'fn': fn, 'fn_prct': 100*fn/total,
                'tp': tp, 'tp_prct': 100*tp/total,
                }
-
-    fmt = f"""
-    Area under ROC curve:                                        {auc:.2f}%
-    True negatives  (correctly predicted normal use):   {tn:6d} ({tn_prct:.2f}%)
-    False positives (incorrectly predicted attacks):    {fp:6d} ({fp_prct:.2f}%)
-    False negatives (incorrectly predicted normal use): {fn:6d} ({fn_prct:.2f}%)
-    True positives  (correctly predicted attacks):      {tp:6d} ({tp_prct:.2f}%)
-    """
-    print(fmt)
+    #TODO extract values from the results dictionary then create the formatted strings
+    # auc = f'''Area under ROC curve:                                        {auc:.2f}%'''
+    # tn  = f'''True negatives  (correctly predicted normal use):   {tn:6d} ({results.get('tn_prct'):.2f}%)'''
+    # fp  = f'''False positives (incorrectly predicted attacks):    {fp:6d} ({fp_prct:.2f}%)'''
+    # fn  = f'''False negatives (incorrectly predicted normal use): {fn:6d} ({fn_prct:.2f}%)'''
+    # tp  = f'''True positives  (correctly predicted attacks):      {tp:6d} ({tp_prct:.2f}%)'''
+    
+    # print(fmt)
 
     return results
